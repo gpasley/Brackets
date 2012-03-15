9cc670ab90f79eee5656ba71b9c36be09cdd5f79
George Pasley <gpasley@3freed.com>
2012-03-14 20:08:15 -0400

    pick sort

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   
   def index
-    @picks = Sheet.count(:group => :round6)
+    @picks = Sheet.count(:group => :round6, :order => :round6)
   end
 end
