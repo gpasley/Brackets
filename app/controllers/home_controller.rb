0094875c178a9ad8e120c2f7f3b5197eb172432a
George Pasley <gpasley@3freed.com>
2013-03-17 22:56:26 -0400

    new login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,14 @@
 class HomeController < ApplicationController
   
   def index
-    @picks = Sheet.count(:group => :round6, :order => :round6, :conditions => {:is_master => false})
-    
-    @sheet = Sheet.where("is_master = ?", true).first
-    
-    @leaders = Sheet.where("is_master = ?", false).order("total_points desc").limit(10)
+    if user_signed_in?
+      @picks = Sheet.count(:group => :round6, :order => :round6, :conditions => {:is_master => false})
+      @sheet = Sheet.where("is_master = ?", true).first
+      @leaders = Sheet.where("is_master = ?", false).order("total_points desc").limit(10)  
+    else
+      @page = 'front'
+      redirect_to new_user_session_path
+    end
   end
+  
 end
