602dbe90d8c482f68112d8e16bb7f04bbd653adf
George Pasley <gpasley@3freed.com>
2014-03-20 13:33:56 -0400

    hide add button when locked

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
       @picks = Sheet.count(:group => :round6, :order => :round6, :conditions => {:is_master => false})
       @sheet = Sheet.where("is_master = ?", true).first
       @leaders = Sheet.where("is_master = ?", false).order("total_points desc").limit(10)  
+      @locked = Sheet.where("is_locked = ?", true).first
     else
       @page = 'front'
       redirect_to new_user_session_path
